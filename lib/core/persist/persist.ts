c26aa67a239a4bd6147c811187bd490159c82ab7
Breno Jesus <breno@inspr.com>
2023-09-21 22:34:09 -0300

    feat: add persist middleware and store impl

--- lib/core/persist/persist.ts
+++ lib/core/persist/persist.ts
@@ -1,9 +1,12 @@
 import { Store } from "../create/create";
 
-interface Storage {
+export interface Storage {
   getItem(key: string): string | null;
   setItem(key: string, value: string): void;
   removeItem(key: string): void;
+
+  // allow store to have an obtional subscribe method so that when store change we notify the storage listener
+  subscribe?: (key: string, callback: (state: string) => void) => void;
 }
 
 interface PersistOptions<T> {
@@ -30,7 +33,136 @@ interface PersistOptions<T> {
 
 export function persist<T>(
   store: Store<T>,
-  options: PersistOptions<T>
+  options: PersistOptions<T> = {
+    key: "store",
+    storage: local(),
+  }
 ): Store<T> {
+  const {
+    key,
+    storage,
+    serialize = JSON.stringify,
+    deserialize = JSON.parse,
+  } = options;
+
+  const value = storage.getItem(key);
+  if (value !== null) {
+    store.set(deserialize(value));
+  }
+
+  store.subscribe((state) => {
+    storage.setItem(key, serialize(state));
+  });
+
+  if (storage.subscribe) {
+    storage.subscribe(key, (state) => {
+      store.set(deserialize(state));
+    });
+  }
+
   return store;
 }
+
+export const memory = (): Storage => {
+  const store: Record<string, string> = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value;
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+  };
+};
+
+export const local = (): Storage => {
+  if (typeof window === "undefined") {
+    return memory();
+  }
+
+  return {
+    getItem: (key) => window.localStorage.getItem(key),
+    setItem: (key, value) => localStorage.setItem(key, value),
+    removeItem: (key) => localStorage.removeItem(key),
+    subscribe: (key, callback) => {
+      addEventListener("storage", (event: StorageEvent) => {
+        if (event.key === key) {
+          if (event.newValue !== null) {
+            return callback(event.newValue);
+          }
+        }
+      });
+    },
+  };
+};
+
+interface SessionStorageOptions {}
+
+export const session = (options: SessionStorageOptions): Storage => {
+  return {
+    getItem: (key) => window.sessionStorage.getItem(key),
+    setItem: (key, value) => sessionStorage.setItem(key, value),
+    removeItem: (key) => sessionStorage.removeItem(key),
+  };
+};
+
+interface CookieStorageOptions {
+  expires?: Date;
+  path?: string;
+  domain?: string;
+  secure?: boolean;
+}
+
+export const cookie = (options: CookieStorageOptions): Storage => {
+  return {
+    getItem: (key) => {
+      const value = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(`${key}=`));
+
+      return value ? value.split("=")[1] : null;
+    },
+    setItem: (key, value) => {
+      const cookie = `${key}=${value}; ${
+        options.expires ? `expires=${options.expires.toUTCString()}; ` : ""
+      }${options.path ? `path=${options.path}; ` : ""}${
+        options.domain ? `domain=${options.domain}; ` : ""
+      }${options.secure ? `secure; ` : ""}`;
+
+      document.cookie = cookie;
+    },
+    removeItem: (key) => {
+      document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT${
+        options.path ? `; path=${options.path}` : ""
+      }${options.domain ? `; domain=${options.domain}` : ""}${
+        options.secure ? "; secure" : ""
+      }`;
+    },
+  };
+};
+
+export const url = (): Storage => {
+  const params = new URLSearchParams(window.location.search);
+
+  return {
+    getItem: (key) => params.get(key),
+    setItem: (key, value) => {
+      params.set(key, value);
+      window.history.replaceState(
+        null,
+        "",
+        `${window.location.pathname}?${params.toString()}`
+      );
+    },
+    removeItem: (key) => {
+      params.delete(key);
+      window.history.replaceState(
+        null,
+        "",
+        `${window.location.pathname}?${params.toString()}`
+      );
+    },
+  };
+};
