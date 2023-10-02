4d63cda937edd2b510c3082c2368b59689696ec6
Breno Jesus <breno@inspr.com>
2023-10-02 09:51:50 -0300

    feat: update useStore to support both SyncStore and AsyncStore

--- lib/react/use-store.ts
+++ lib/react/use-store.ts
@@ -1,22 +1,22 @@
 import { useEffect, useRef, useSyncExternalStore } from "react";
-import { Subscribable, Unwrappable } from "../core/types";
+import { AsyncStore, SyncStore } from "../core/create/create";
 
-type StoreType<T> = Subscribable<T> & Unwrappable<T> & PromiseLike<T>;
+type StoreType<T> = SyncStore<T> | AsyncStore<T>;
 
 export function useStore<T>(state: StoreType<T>): [T, boolean];
 export function useStore<T>(state: StoreType<T>): T;
 export function useStore<T>(state: StoreType<T>): T | [T, boolean] {
   let isHydrated = useRef(false).current;
-  const storeState = useSyncExternalStore(
-    state.subscribe,
-    state.unwrap,
-    state.unwrap
-  );
+  const storeState = useSyncExternalStore(state.subscribe, state.unwrap);
 
   useEffect(() => {
-    state.then(() => {
+    if (state.type === "async") {
+      state.then(() => {
+        isHydrated = true;
+      });
+    } else {
       isHydrated = true;
-    });
+    }
   }, [state]);
 
   return isHydrated ? [storeState, true] : storeState;
