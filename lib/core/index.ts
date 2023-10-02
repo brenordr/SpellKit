f1caf16b60182221e1e8915366c7c33b5f594110
Breno Jesus <breno@inspr.com>
2023-10-01 21:01:46 -0300

    refactor(core): change explicit exports to wildcard exports for core modules

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -1,5 +1,5 @@
-export { actions } from "./actions/actions";
-export { channel } from "./channel/channel";
-export { computed } from "./computed/computed";
-export { create, type Store } from "./create/create";
-export { lens } from "./lens/lens";
+export * from "./actions/actions";
+export * from "./channel/channel";
+export * from "./computed/computed";
+export * from "./create/create";
+export * from "./lens/lens";
