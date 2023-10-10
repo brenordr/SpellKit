24e816084054c7d365c43cf2d8c14bf99fd85a76
Breno Jesus <breno@inspr.com>
2023-10-10 07:27:38 -0300

    fix(actions): amend Action and Actions types to resolve type mismatch

--- lib/core/actions/index.ts
+++ lib/core/actions/index.ts
@@ -1,7 +1,7 @@
 import { Publishable, Subscribable, Unwrappable } from "..";
 import { Store } from "../store";
 
-export type Action<T> = (...args: any[]) => any;
+export type Action<T> = (...args: any[]) => any | void;
 
 export type ActionsCreator<T, A extends { [key: string]: Action<T> }> = (
   unwrap: Unwrappable<T>["unwrap"],
@@ -11,23 +11,12 @@ export type ActionsCreator<T, A extends { [key: string]: Action<T> }> = (
 
 export type Actions<T, A extends { [key: string]: Action<T> }> = {
   [K in keyof A]: (...args: Parameters<A[K]>) => any;
-};
+} & { [key: string]: Action<T> }; // Adding an index signature here
 
 export function actions<T, A extends { [key: string]: Action<T> }>(
   store: Store<T>,
   creator: ActionsCreator<T, A>
 ): Actions<T, A> {
   const actionHandlers = creator(store.unwrap, store.publish, store.subscribe);
-  const actionHandlersWithPublish: Partial<Actions<T, A>> = {};
-
-  for (const key in actionHandlers) {
-    if (Object.prototype.hasOwnProperty.call(actionHandlers, key)) {
-      const action = actionHandlers[key];
-      actionHandlersWithPublish[key] = (...args: any[]) => {
-        action(...args);
-      };
-    }
-  }
-
-  return actionHandlersWithPublish as Actions<T, A>;
+  return actionHandlers as Actions<T, A>;
 }
